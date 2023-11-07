8287da3857c1b9fc4bc9229352cbe72dc1b56540
dorinahashicorp <94868032+dorinahashicorp@users.noreply.github.com>
2023-11-07 11:14:10 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
+terraform {
 required_providers {
     hcp = {
       source = "hashicorp/hcp"
       version = "0.63.0"
     }
   }
-
+}
 provider "hcp" {
   client_id = var.HCP_CLIENT_ID
   client_secret = var.HCP_CLIENT_SECRET
