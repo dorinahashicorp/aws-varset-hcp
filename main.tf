2cc79fab9956466b8ce9f1077450c964e2b005be
dorinahashicorp <94868032+dorinahashicorp@users.noreply.github.com>
2023-11-07 14:25:48 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,19 +4,11 @@ required_providers {
       source = "hashicorp/hcp"
       version = "0.63.0"
     }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.24.0"
-    }
 }
 }
 
 data "hcp_vault_secrets_app" "Learning-app" {
   app_name = "Learning-app"
-}
-
-provider "aws" {
-  region = "us-east-2"
 }
 
 provider "hcp" {
@@ -32,14 +24,6 @@ variable "HCP_CLIENT_SECRET" {
   type = string
 }
 
-resource "aws_instance" "dorina-vm" {
-  ami           = "ami-00448a337adc93c05" 
-  instance_type = "t2.micro" 
-  tags = {
-    Name = "dorina-vm"
-  }
-}
-
-output "public_ip" {
-  value = aws_instance.dorina-vm.public_ip
+output "dorina-test" {
+  value = data.hcp_vault_secrets_app.Learning-app
 }
