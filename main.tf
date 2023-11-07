39a8c1836c0395f5c30d731779beecc70e9ba92c
dorinahashicorp <94868032+dorinahashicorp@users.noreply.github.com>
2023-11-07 15:39:17 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,24 +7,34 @@ required_providers {
 }
 }
 
-data "hcp_vault_secrets_app" "Learning-app" {
-  app_name = "Learning-app"
+data "hcp_vault_secrets_secret" "access_key" {
+  app_name    = "Learning-app"
+  secret_name = "AWS_ACCESS_KEY_ID"
 }
 
-provider "hcp" {
-  client_id = var.HCP_CLIENT_ID
-  client_secret = var.HCP_CLIENT_SECRET
+data "hcp_vault_secrets_secret" "secret_key" {
+  app_name    = "Learning-app"
+  secret_name = "AWS_SECRET_ACCESS_KEY"
 }
 
-variable "HCP_CLIENT_ID" {
-  type = string
+resource "tfe_variable" "access_key" {
+  key             = "AWS_ACCESS_KEY_ID"
+  value           = data.hcp_vault_secrets_secret.access_key.secret_value
+  category        = "environment"
+  description     = "The AWS Access Key"
+  variable_set_id = tfe_variable_set.test.id
 }
 
-variable "HCP_CLIENT_SECRET" {
-  type = string
+resource "tfe_variable" "secret_key" {
+  key             = "AWS_ACCESS_KEY_ID"
+  value           = data.hcp_vault_secrets_secret.secret_key.secret_value
+  category        = "environment"
+  description     = "The AWS Secret Key"
+  variable_set_id = tfe_variable_set.test.id
 }
 
-output "dorina-test" {
-  value = data.hcp_vault_secrets_app.Learning-app
-  sensitive = true
+resource "tfe_variable_set" "test" {
+  name          = "Dorinas AWS Creds"
+  description   = "Env Variables for Dorinas AWS creds"
+  organization  = "Infragoose"
 }
